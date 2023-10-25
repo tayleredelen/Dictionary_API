7c941849805c59349b984e00b5387918dc5bcf96
tayleredelen <taylerledelen@gmail.com>
2023-10-25 13:22:06 -0500

    Day 30 -- complete

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("dictionary.csv")
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/api/v1/<word>")
+@app.route("/api/v1/<word>/")
 def api(word):
-    file = pd.read_csv("dictionary.csv")
-    definition = file.loc[file[word] == word]
+    definition = df.loc[df["word"] == word]['definition'].squeeze()
     result_dictionary = {"word": word, "definition": definition}
     return result_dictionary
 
